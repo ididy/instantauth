91af8991550165928913678dac9ab08b374d08ba
Jeong YunWon <jeong@youknowone.org>
2013-07-24 10:34:05 +0900

    keep the order of json coder

--- python/instantauth/coders/json.py
+++ python/instantauth/coders/json.py
@@ -2,11 +2,14 @@
 from __future__ import absolute_import
 
 import json
+import collections
 from . import BaseCoder
+
+OrderedJsonDecoder = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
 
 class JsonCoder(BaseCoder):
     def encode(self, data):
         return json.dumps(data, separators=(',',':')) # compact form
 
     def decode(self, data):
-        return json.loads(data)
+        return OrderedJsonDecoder.decode(data)
